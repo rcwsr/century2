0d90d9d98f023fd0d7a7d7168ddb074280c29171
Robin Cawser <robincawser@gmail.com>
2014-09-24 20:48:52 +0100

    Remove fosuserbundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,9 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
             new Century\CenturyBundle\CenturyCenturyBundle(),
             new Braincrafted\Bundle\BootstrapBundle\BraincraftedBootstrapBundle(),
+            new Snc\RedisBundle\SncRedisBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
